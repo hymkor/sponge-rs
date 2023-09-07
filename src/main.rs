74a79d56c6e34c6d619a97d38a4b3061deb940f8
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-07 21:22:09 +0900

    Do not use std::io::BufRead/Write

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use std::io::BufRead;
 use std::io::Write;
+use std::io::Read;
 
 fn mains(args: std::env::Args) -> Result<(),Box<dyn std::error::Error>> {
     if let Some(original) = args.skip(1).next() {
         let tmp_name = original.clone() + ".sponge";
         {
-            let fd = std::fs::File::create(&tmp_name)?;
-            let mut bw = std::io::BufWriter::new(fd);
-            let reader = std::io::BufReader::new(std::io::stdin());
-            for line in reader.lines() {
-                bw.write(&line?.as_bytes())?;
-                bw.write(&[b'\n'])?;
+            let mut w = std::fs::File::create(&tmp_name)?;
+            let stdin = std::io::stdin();
+            let mut r = stdin.lock();
+            let mut buffer = [0u8;1024];
+            loop{
+                let n = r.read(&mut buffer)?;
+                if n <= 0 {
+                    break;
+                }
+                w.write_all(&buffer[0..n])?;
             }
         }
         std::fs::rename(tmp_name,original)?;
