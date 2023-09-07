f58e9c939b69cb0d244340dded5e783cfef51745
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-07 21:39:03 +0900

    decreate indent with using let and match

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,31 @@
-use std::io::Write;
 use std::io::Read;
+use std::io::Write;
 
-fn mains(args: std::env::Args) -> Result<(),Box<dyn std::error::Error>> {
-    if let Some(original) = args.skip(1).next() {
-        let tmp_name = original.clone() + ".sponge";
-        {
-            let mut w = std::fs::File::create(&tmp_name)?;
-            let stdin = std::io::stdin();
-            let mut r = stdin.lock();
-            let mut buffer = [0u8;1024];
-            loop{
-                let n = r.read(&mut buffer)?;
-                if n <= 0 {
-                    break;
-                }
-                w.write_all(&buffer[0..n])?;
-            }
+fn mains(args: std::env::Args) -> std::io::Result<()> {
+    let original = match args.skip(1).next() {
+        Some(original) => original,
+        None => {
+            return Err(
+                std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "file is not specified"))
         }
-        std::fs::rename(tmp_name,original)?;
+    };
+    let tmp_name = original.clone() + ".sponge";
+    {
+        let stdin = std::io::stdin();
+        let mut r = stdin.lock();
+        let mut buffer = [0u8;1024];
+        let mut w = std::fs::File::create(&tmp_name)?;
+        loop{
+            let n = r.read(&mut buffer)?;
+            if n <= 0 {
+                break;
+            }
+            w.write_all(&buffer[0..n])?;
+        }
     }
+    std::fs::rename(tmp_name,original)?;
     return Ok(())
 }
 
