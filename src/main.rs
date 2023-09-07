2fcdee3396cb063e3c2efa2ac2ff03947f9bc749
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-08 02:09:13 +0900

    Implement error: TooManyFilenames

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,19 @@ use thiserror::Error;
 enum OurError {
     #[error("filename is not specified")]
     NoFileName,
+    #[error("too many filenames")]
+    TooManyFileNames,
 }
 
-fn mains(args: std::env::Args) -> Result<(),Box<dyn std::error::Error>> {
+fn mains(args: &mut std::env::Args) -> Result<(),Box<dyn std::error::Error>> {
     let original = match args.skip(1).next() {
         Some(original) => original,
         None => { return Err(Box::new(OurError::NoFileName)) },
     };
+    if let Some(_) = args.next() {
+        return Err(Box::new(OurError::TooManyFileNames))
+    }
+
     let tmp_name = format!("{}.sponge",&original);
     {
         let mut r = std::io::stdin();
@@ -26,7 +32,7 @@ fn mains(args: std::env::Args) -> Result<(),Box<dyn std::error::Error>> {
 }
 
 fn main(){
-    if let Err(err) = mains(std::env::args()) {
+    if let Err(err) = mains(&mut std::env::args()) {
         eprintln!("{}",err);
         std::process::exit(1);
     }
