d4911732fa7c3ea9ad4295ec023ee582653de2ef
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-08 19:51:24 +0900

    ourerror: use tuple structure

--- src/ourerror.rs
+++ src/ourerror.rs
@@ -1,18 +1,14 @@
 #[derive(Debug)]
-struct OurError {
-    msg: String,
-}
+struct OurError(String);
 
 impl std::error::Error for OurError {}
 
 impl std::fmt::Display for OurError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.msg)
+        write!(f, "{}", self.0)
     }
 }
 
 pub fn new(msg: &str) -> Box<dyn std::error::Error> {
-    return Box::new(OurError {
-        msg: String::from(msg),
-    });
+    return Box::new(OurError(String::from(msg)));
 }
