08e698e280eb96ce2ebe7a9761ea9ba1a08250ba
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-08 19:44:24 +0900

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,39 @@
 mod ourerror;
 
 fn sponge(original: String) -> std::io::Result<()> {
-    let tmp_name = format!("{}.sponge",&original);
+    let tmp_name = format!("{}.sponge", &original);
     {
         let mut r = std::io::stdin();
         let mut w = std::fs::File::create(&tmp_name)?;
-        std::io::copy(&mut r,&mut w)?;
+        std::io::copy(&mut r, &mut w)?;
     }
     if std::path::Path::new(&original).try_exists()? {
-        let backup = format!("{}~",&original);
-        std::fs::rename(&original,backup)?;
+        let backup = format!("{}~", &original);
+        std::fs::rename(&original, backup)?;
     }
-    std::fs::rename(tmp_name,original)?;
-    return Ok(())
+    std::fs::rename(tmp_name, original)?;
+    return Ok(());
 }
 
-fn mains() -> Result<(),Box<dyn std::error::Error>> {
+fn mains() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = std::env::args();
     if let None = args.next() {
-        return Err(ourerror::new("filename is not specified") )
+        return Err(ourerror::new("filename is not specified"));
     }
     let original = match args.next() {
         Some(original) => original,
-        None => return Err(ourerror::new("filename is not specified") )
+        None => return Err(ourerror::new("filename is not specified")),
     };
     if let Some(_) = args.next() {
-        return Err(ourerror::new("too many filenames") )
+        return Err(ourerror::new("too many filenames"));
     }
     sponge(original)?;
-    return Ok(())
+    return Ok(());
 }
 
-fn main(){
+fn main() {
     if let Err(err) = mains() {
-        eprintln!("{}",err);
+        eprintln!("{}", err);
         std::process::exit(1);
     }
 }
--- src/ourerror.rs
+++ src/ourerror.rs
@@ -7,10 +7,12 @@ impl std::error::Error for OurError {}
 
 impl std::fmt::Display for OurError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f,"{}",self.msg)
+        write!(f, "{}", self.msg)
     }
 }
 
 pub fn new(msg: &str) -> Box<dyn std::error::Error> {
-    return Box::new(OurError{ msg:String::from(msg) })
+    return Box::new(OurError {
+        msg: String::from(msg),
+    });
 }
